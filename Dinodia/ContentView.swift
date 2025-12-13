587212c1aa177617fa6c12b12c65da653feaf39a
Nivedit Gupta <niveditgupta@hotmail.co.uk>
2025-12-13 20:16:45 +0000

    Initial Commit

---
+++ Dinodia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dinodia
+//
+//  Created by Nivedit Gupta on 13/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dinodia/DinodiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinodiaApp.swift
+//  Dinodia
+//
+//  Created by Nivedit Gupta on 13/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct DinodiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
